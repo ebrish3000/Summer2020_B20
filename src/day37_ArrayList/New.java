c67cd69689a9ca6137a466820afeb2b222eb8c54
ebrish3000 <ebrish3000@gmail.com>
2020-08-02 13:25:52 -0700

    Day37_ArrayList

--- src/day37_ArrayList/New.java
+++ src/day37_ArrayList/New.java
@@ -3,6 +3,6 @@ package day37_ArrayList;
 public class New {
     public static void main(String[] args) {
         System.out.println("it is a great help");
-        // comment1
+        // comment1S          DHHESBMFNAA
     }
 }
